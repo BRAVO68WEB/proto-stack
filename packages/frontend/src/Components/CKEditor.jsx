0178a5b549ddb5bcb08377ef515b81648fca87fc
sanyog <sanyogchangmai29@gmail.com>
2022-09-04 00:20:49 +0000

    implemented create blog

--- packages/frontend/src/Components/CKEditor.jsx
+++ packages/frontend/src/Components/CKEditor.jsx
@@ -1,9 +1,10 @@
 import { Box } from "@chakra-ui/react";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function CKeditor({ onChange, editorLoaded, name, value }) {
     const editorRef = useRef();
     const { CKEditor, ClassicEditor } = editorRef.current || {};
+
     useEffect(() => {
         editorRef.current = {
             CKEditor: require("@ckeditor/ckeditor5-react").CKEditor,
--- packages/frontend/src/Pages/CreateBlog.jsx
+++ packages/frontend/src/Pages/CreateBlog.jsx
@@ -1,12 +1,22 @@
 import { useEffect, useState, useRef } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import { Container, Box, Button } from "@chakra-ui/react";
+import { useToast } from '@chakra-ui/react';
 import CKeditor from "../Components/CKEditor";
 import Appbar from "../Components/Appbar";
+import { htmlToText } from "html-to-text";
+import parse from "html-react-parser";
+import axios from '../helpers/axios';
 
 const CreateBlog = () => {
 
     const [editorLoaded, setEditorLoaded] = useState(false);
     const [data, setData] = useState("");
+    const [btnText, setBtnText] = useState("Submit");
+
+    const navigate = useNavigate();
+
+    const toast = useToast();
 
     useEffect(() => {
         setEditorLoaded(true);
@@ -14,7 +24,47 @@ const CreateBlog = () => {
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(data);
+        console.log(JSON.stringify(data));
+        let raw = {
+            "content": parse(data),
+        };
+
+        axios.post("/blog/create", raw)
+            .then((result) => {
+                console.log(result);
+                if (result.data.status == true) {
+                    console.log(result);
+                    toast({
+                        title: 'Submitted successfully.',
+                        description: "Submitted successfully.",
+                        status: 'success',
+                        duration: 9000,
+                        isClosable: true,
+                    })
+                    navigate("/");
+                }
+                else {
+                    setBtnText("Submit");
+                    toast({
+                        title: 'Failed to Submit.',
+                        description: result.message,
+                        status: 'error',
+                        duration: 9000,
+                        isClosable: true,
+                    })
+                }
+            })
+            .catch((error) => {
+                // console.log('error', error);
+                setBtnText("Submit");
+                toast({
+                    title: 'Failed to Submit.',
+                    description: "Something went wrong. Try again.",
+                    status: 'error',
+                    duration: 9000,
+                    isClosable: true,
+                })
+            });
     }
 
     return (
@@ -24,11 +74,13 @@ const CreateBlog = () => {
                 <form onSubmit={handleSubmit}>
                     <CKeditor
                         name="description"
-                        onChange={() => {
+                        onChange={(data) => {
                             setData(data);
                         }}
                         editorLoaded={editorLoaded}
                     />
+
+                    {/* {JSON.stringify(data)} */}
                     <Button type="submit" mt="3" maxW="9rem" colorScheme='teal'>Submit</Button>
                 </form>
             </Container>
